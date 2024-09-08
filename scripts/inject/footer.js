b2bd57876753724220ff78b2a8a649433a4e22e5
saphire-gamez <saphiregamezz@gmail.com>
2024-09-08 18:56:40 -0400

    Update footer.js

--- scripts/inject/footer.js
+++ scripts/inject/footer.js
@@ -4,23 +4,14 @@ document.write(`
             <p class="footer-title">Other</p>
             <a class="footer-a" href="/suggestions.html">Suggestions</a>
             <a class="footer-a" href="/issue.html">Report Issues</a>
-            <a class="footer-a" href="/links.html">More Links</a>
         </div>
         <div class="footer-2">
             <p class="footer-title">Developers</p>
-            <a class="footer-a" href="https://github.com/MyBooty165" target="_blank">MyBooty165 (X-88)</a>
+            <a class="footer-a" href="" target="_blank">SaphireGames TM</a>
         </div>
         <div class="footer-3">
             <p class="footer-title">Community</p>
-            <a class="footer-a" href="https://twitter.com/cybriatech_" target="_blank">Twitter (X)</a>
-            <a class="footer-a" href="https://tiktok.com/@cybriatech" target="_blank">TikTok</a>
-            <a class="footer-a" href="https://discord.gg/zhsmmNNtJT" target="_blank">Discord</a>
-        </div>
-        <div class="footer-4">
-            <p class="footer-title">Sources</p>
-            <a class="footer-a" href="https://github.com/CybriaTech/CybriaGames" target="_blank">GitHub Repo</a>
-            <a class="footer-a" href="https://github.com/CybriaTech/CybriaGames-Assets" target="_blank">Game Assets</a>
-            <a class="footer-a" href="https://cybriagames-old.pages.dev/" target="_blank">Old Website</a>
+            <a class="footer-a" href="https://ocps-schools.unibutton.com" target="_blank">Saphire Games</a>
         </div>
     </footer>
 `);
